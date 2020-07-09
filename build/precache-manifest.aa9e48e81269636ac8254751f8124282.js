b0c7b6242ead8c4bc0ec72d7d99833531eb393cb
Pete Brennan <pweda64@hotmail.com>
2020-07-09 21:38:41 +0800

    added build

---
+++ build/precache-manifest.aa9e48e81269636ac8254751f8124282.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6de43d16e6f7ee308cca0beba044bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10cfa4a3b7f212fa9006",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4a198ff3451117095919",
+    "url": "/static/css/main.dfa6d8c0.chunk.css"
+  },
+  {
+    "revision": "10cfa4a3b7f212fa9006",
+    "url": "/static/js/2.b5e793f6.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.b5e793f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a198ff3451117095919",
+    "url": "/static/js/main.087904cd.chunk.js"
+  },
+  {
+    "revision": "a5639207fd9f89330370",
+    "url": "/static/js/runtime-main.d29be8ab.js"
+  }
+]);
