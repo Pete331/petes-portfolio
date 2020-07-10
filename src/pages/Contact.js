705bb767a61d4a21c33a81695ee0a76dc82c02e0
Peter Brennan <pweda64@hotmail.com>
2020-07-10 16:09:28 +0800

    last chance

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,153 +1,79 @@
-import React, { useState } from "react";
+import React from 'react'
+// import { navigate } from 'gatsby-link'
+// import Layout from '../layout'
+import { Route } from "react-router-dom";
 
-const encode = (data) => {
+function encode(data) {
   return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
 }
 
+export default function Contact() {
+  const [state, setState] = React.useState({})
 
-function Contact() {
-  const [nameValue, setNameValue] = useState("");
-  const [emailValue, setEmailValue] = useState("");
-  const [phoneValue, setPhoneValue] = useState("");
-  const [messageValue, setMessageValue] = useState("");
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
 
-  // function HandleSubmit(event) {
-  //   console.log(event);
-  // }
-
-  function handleSubmit(e) {
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", "name": nameValue, "email": emailValue })
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...state,
+      }),
     })
-      .then(() => alert("Success!"))
-      .catch(error => alert(error));
-
-    e.preventDefault();
-  };
+      .then( console.log("yay"))
+      .catch((error) => alert(error))
+  }
 
   return (
-    <div className="container">
-      <div className="row bg-white border border-secondary">
-        <div className="col-sm-12 p-4">
-          <div className="border-bottom border-secondary mb-3">
-            <h2 className="text-info">Contact</h2>
-          </div>
-          <h5>Submit the below form to recieve a copy of my résumé</h5>
-
-          {/* <!-- Begin Mailchimp Signup Form --> */}
-          <div id="mc_embed_signup">
-            <form
-              // action="https://github.us3.list-manage.com/subscribe/post?u=64a019defcb78cebd2fa23e6a&amp;id=e5f8fe46ea"
-              method="post"
-              // id="mc-embedded-subscribe-form"
-              name="contact"
-              // className="validate"
-              // target="_blank"
-              data-netlify="true"
-              // novalidate
-              onSubmit={handleSubmit}
-            >
-              <div id="mc_embed_signup_scroll">
-                <div className="mc-field-group mt-3">
-                  <label htmlFor="mce-NAME">Name </label>
-                  <input
-                    type="text"
-                    value={nameValue}
-                    onChange={(e) => {
-                      setNameValue(e.target.value);
-                    }}
-                    name="NAME"
-                    className="form-control"
-                    // id="mce-NAME"
-                  />
-                </div>
-                <div className="mc-field-group mt-3">
-                  <label htmlFor="mce-EMAIL">
-                    Email <span className="asterisk">*</span>
-                  </label>
-                  <input
-                    type="email"
-                    value={emailValue}
-                    onChange={(e) => {
-                      setEmailValue(e.target.value);
-                    }}
-                    name="EMAIL"
-                    className="form-control required email"
-                    // id="mce-EMAIL"
-                  />
-                </div>
-                <div className="mc-field-group size1of2 mt-3">
-                  <label htmlFor="mce-PHONE">Phone </label>
-                  <input
-                    type="text"
-                    name="PHONE"
-                    className="form-control"
-                    value={phoneValue}
-                    onChange={(e) => {
-                      setPhoneValue(e.target.value);
-                    }}
-                    // id="mce-PHONE"
-                  />
-                </div>
-                <div className="mc-field-group mt-3">
-                  <label htmlFor="mce-MESSAGE">Message </label>
-                  <input
-                    type="text"
-                    value={messageValue}
-                    onChange={(e) => {
-                      setMessageValue(e.target.value);
-                    }}
-                    name="MESSAGE"
-                    className="form-control"
-                    // id="mce-MESSAGE"
-                  />
-                </div>
-                {/* <div id="mce-responses" className="clear">
-                  <div
-                    className="response"
-                    id="mce-error-response"
-                    style={{ display: "none" }}
-                  ></div>
-                  <div
-                    className="response"
-                    id="mce-success-response"
-                    style={{ display: "none" }}
-                  ></div>
-                </div> */}
-                {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
-                {/* <div
-                  style={{ position: "absolute", left: "-5000px" }}
-                  aria-hidden="true"
-                >
-                  <input
-                    type="text"
-                    name="b_64a019defcb78cebd2fa23e6a_e5f8fe46ea"
-                    tabIndex="-1"
-                    defaultValue=""
-                  />
-                </div> */}
-                {/* <div className="clear"> */}
-                  <button
-                    type="submit"
-                    // value="Submit"
-                    // name="subscribe"
-                    // id="mc-embedded-subscribe"
-                    className="button btn btn-info mt-3"
-                    // onClick={(e) => {
-                    //   HandleSubmit(e.target)}}
-                  >Submit</button>
-                {/* </div> */}
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
+    <div>
+      <h1>Contact</h1>
+      <form
+        name="contact"
+        method="post"
+        action="/thanks/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
+      >
+        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <label>
+            Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your name:
+            <br />
+            <input type="text" name="name" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your email:
+            <br />
+            <input type="email" name="email" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message:
+            <br />
+            <textarea name="message" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </div>
-  );
+  )
 }
-
-export default Contact;
