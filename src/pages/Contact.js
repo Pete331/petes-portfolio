9b2936006ff27300a535c8beefd7fb251259a94d
Peter Brennan <pweda64@hotmail.com>
2020-07-11 09:38:15 +0800

    updated

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,75 +3,100 @@ import React from "react";
 
 function encode(data) {
   return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&')
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
 }
 
 export default function Contact() {
-  const [state, setState] = React.useState({})
+  const [state, setState] = React.useState({});
 
   const handleChange = (e) => {
-    setState({ ...state, [e.target.name]: e.target.value })
-  }
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    const form = e.target
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
-        'form-name': form.getAttribute('name'),
+        "form-name": form.getAttribute("name"),
         ...state,
       }),
     })
       .then(alert("Sending"))
-      .catch((error) => alert(error))
-  }
+      .catch((error) => alert(error));
+  };
 
   return (
-    <div>
-      <h1>Contact</h1>
-      <form
-        name="contact"
-        method="post"
-        action="/thanks/"
-        data-netlify="true"
-        // data-netlify-honeypot="bot-field"
-        onSubmit={handleSubmit}
-      >
-        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-        <input type="hidden" name="form-name" value="contact" />
-        <p hidden>
-          <label>
-            Don’t fill this out: <input name="bot-field" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your name:
-            <br />
-            <input type="text" name="name" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your email:
-            <br />
-            <input type="email" name="email" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message:
-            <br />
-            <textarea name="message" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+    <div className="container">
+      <div className="row bg-white border border-secondary">
+        <div className="col-sm-12 p-4">
+          <div className="border-bottom border-secondary mb-3">
+            <h2 className="text-info">Contact</h2>
+          </div>
+          <h5>Submit the below form to recieve a copy of my résumé</h5>
+
+          <form
+            name="contact"
+            method="post"
+            action="/thanks/"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            onSubmit={handleSubmit}
+          >
+            {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+            <input type="hidden" name="form-name" value="contact" />
+            <p hidden>
+              <label>
+                Don’t fill this out:{" "}
+                <input name="bot-field" onChange={handleChange} />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your name:
+                <br />
+                <input
+                  className="form-control"
+                  type="text"
+                  name="name"
+                  onChange={handleChange}
+                />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your email:
+                <br />
+                <input
+                  className="form-control"
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                />
+              </label>
+            </p>
+            <p>
+              <label>
+                Message:
+                <br />
+                <textarea
+                  className="form-control"
+                  name="message"
+                  onChange={handleChange}
+                />
+              </label>
+            </p>
+            <p>
+              <button className="button btn btn-info mt-3" type="submit">
+                Submit
+              </button>
+            </p>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
