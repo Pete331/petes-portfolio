124e99dc58dd7fa123cb19dbcd3f75f76e9a5909
Peter Brennan <pweda64@hotmail.com>
2020-07-11 08:03:27 +0800

    try again

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,58 +1,77 @@
 import React from "react";
 // import { navigateTo } from "gatsby-link";
 
-const encode = (data) => {
+function encode(data) {
   return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
 }
 
-class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { name: "", email: "", message: "" };
+export default function Contact() {
+  const [state, setState] = React.useState({})
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value })
   }
 
-  /* Here’s the juicy bit for posting the form submission */
-
-  handleSubmit = e => {
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...this.state })
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...state,
+      }),
     })
-      .then(() => alert("Success!"))
-      .catch(error => alert(error));
+      .then(alert("Sending"))
+      .catch((error) => alert(error))
+  }
 
-    e.preventDefault();
-  };
-
-  handleChange = e => this.setState({ [e.target.name]: e.target.value });
-
-  render() {
-    const { name, email, message } = this.state;
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <p>
+  return (
+    <div>
+      <h1>Contact</h1>
+      <form
+        name="contact"
+        method="post"
+        action="/thanks/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
+      >
+        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
           <label>
-            Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+            Don’t fill this out: <input name="bot-field" onChange={handleChange} />
           </label>
         </p>
         <p>
           <label>
-            Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+            Your name:
+            <br />
+            <input type="text" name="name" onChange={handleChange} />
           </label>
         </p>
         <p>
           <label>
-            Message: <textarea name="message" value={message} onChange={this.handleChange} />
+            Your email:
+            <br />
+            <input type="email" name="email" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message:
+            <br />
+            <textarea name="message" onChange={handleChange} />
           </label>
         </p>
         <p>
           <button type="submit">Send</button>
         </p>
       </form>
-    );
-  }
+    </div>
+  )
 }
-export default Contact
